0a381252b0bfdcd6ad8797eba48b5c40c71b1574
Edson Perez <grreenpm@gmail.com>
2026-03-02 20:37:06 -0600

    P4: estructura de usuarios y tabla doctores

--- modules/auth.py
+++ modules/auth.py
@@ -1,2 +1,9 @@
 """Módulo de autenticación — Responsable: P4"""
 # TODO Sprint 1 - P4
+# Lista de usuarios permitidos según su función
+Roles = ["admin", "enfermero", "doctor"]
+
+def verificar_acceso(usuario, contraseña):
+    # Esta línea ahora está limpia y sin errores de comillas
+    print(f"Intentando entrar con el usuario: {usuario}")
+
